43c46349dc8f0726f0abf6687500d638fcc26e2b
pojoin <hechuangqiang@gmail.com>
2020-03-02 10:09:46 +0800

    update

--- app/__init__.py
+++ app/__init__.py
@@ -81,22 +81,7 @@ def register_shell_context(app):
 
 
 def register_errors(app):
-    @app.errorhandler(400)
-    def bad_request(e):
-        return render_template('errors/400.html'), 400
-
-    @app.errorhandler(404)
-    def page_not_found(e):
-        return render_template('errors/404.html'), 404
-
-    @app.errorhandler(500)
-    def internal_server_error(e):
-        return render_template('errors/500.html'), 500
-
-    @app.errorhandler(CSRFError)
-    def handle_csrf_error(e):
-        return render_template('errors/400.html', description=e.description), 400
-
+    pass
 
 def register_request_handlers(app):
     @app.after_request
